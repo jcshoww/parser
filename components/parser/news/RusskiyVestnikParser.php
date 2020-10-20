b72dcaaae8fa30ed28eab131672e001779d9f889
jcshoww <corveynreigns@gmai.com>
2020-10-20 16:28:38 +0300

    SECREATEIO-10: Убрал дублирование заголовков и текста

--- components/parser/news/RusskiyVestnikParser.php
+++ components/parser/news/RusskiyVestnikParser.php
@@ -137,6 +137,7 @@ class RusskiyVestnikParser implements ParserInterface
     {
         $image = '';
         $description = '';
+        $h2 = '';
         foreach ($item->childNodes as $childNode) {
             if (self::isImageType($childNode) && $childNode->getAttribute('src') === 'images/2.gif') {
                 continue;
@@ -149,9 +150,19 @@ class RusskiyVestnikParser implements ParserInterface
                 continue;
             }
 
-            /** Get post title */
+            /** Get extremely bad formatted post title */
             if ($childNode->tagName === $titleTag) {
-                $title = $childNode->textContent;
+                foreach ($childNode->childNodes as $subnode) {
+                    if (self::isText($subnode) && self::hasActualText($subnode->textContent)) {
+                        $title = $subnode->textContent;
+                    } elseif (isset($subnode->tagName) && $subnode->tagName === 'font') {
+                        if (empty($title) === true) {
+                            $title = $subnode->textContent;
+                        } else {
+                            $h2 = $subnode->textContent;
+                        }
+                    }
+                }
                 continue;
             }
 
@@ -182,6 +193,11 @@ class RusskiyVestnikParser implements ParserInterface
         /** @var NewsPost */
         $post = new NewsPost(static::class, $title, htmlspecialchars_decode($description), $createdAt, $link, $image);
 
+        if (! empty($h2) === true) {
+            $post->addItem(new NewsPostItem(NewsPostItem::TYPE_HEADER, $h2,
+                null, null, 2));
+        }
+
         self::parsePostDetails($post, $detailCrawler);
 
         self::$posts[] = $post;
@@ -198,60 +214,14 @@ class RusskiyVestnikParser implements ParserInterface
     public static function parsePostDetails(NewsPost $post, Crawler $crawler): void
     {
         self::removeNodes($crawler, '//br');
-        $fullTextBlock = $crawler->filter('div[align=justify]')->getNode(0);
+        self::removeNodes($crawler, '//div[@align="justify"]//b[1]');
+        self::removeNodes($crawler, '//div[@align="justify"]//img[1]');
+        if (! empty($post->image) === true) {
+            self::removeNodes($crawler, '//div[@align="justify"]//img[1]');
+        }
+        $fullTextBlock = $crawler->filterXPath('//div[@align="justify"]')->getNode(0);
         foreach ($fullTextBlock->childNodes as $node) {
-            if (self::isImageType($node) && $node->getAttribute('src') === 'images/2.gif') {
-                continue;
-            }
             self::parseNode($post, $node);
-        }
-    }
-
-    /**
-     * Function appends NewsPostItem objects to NewsPost with additional post data
-     * 
-     * @param NewsPost $post
-     * @param Element $content
-     * @param string $content
-     * 
-     * @return void
-     */
-    public static function appendPostAdditionalData(NewsPost $post, Element $content, string $preview): void
-    {
-        $crawler = new Crawler($preview);
-
-        // Get item detail image
-        $image = $crawler->filterXPath('//div[contains(@class, "K2FeedImage")]//img')->first();
-        if ($image->count() === 1) {
-            $detailImage = self::cleanUrl($image->attr('src') ?: '');
-            $post->addItem(new NewsPostItem(NewsPostItem::TYPE_IMAGE, null, $detailImage));
-        }
-
-        // Get item text
-        $introText = $crawler->filterXPath('//div[contains(@class, "K2FeedIntroText")]')->getNode(0);
-        if (! empty($introText) === true) {
-            foreach ($introText->childNodes as $node) {
-                self::parseNode($post, $node);
-            }
-        }
-
-        //Get full text block
-        $fullTextBlock = $crawler->filterXPath('//div[contains(@class, "K2FeedFullText")]')->getNode(0);
-        if (! empty($fullTextBlock) === true) {
-            //Parsing each child node of full text block
-            foreach ($fullTextBlock->childNodes as $node) {
-                self::parseNode($post, $node);
-            }
-        }
-
-        //Get item video
-        $videoBlock = $content->findOne('.itemVideoBlock');
-        if (! empty($videoBlock) === true) {
-            $video = $videoBlock->findOne('iframe');
-            $video = trim($video->getAttribute('src') ?: '');
-            $videoId = preg_replace("/(.+)(\/)([^\/\?]+)([\?]{0,1})(.*)$/", '$3', $video);
-            $post->addItem(new NewsPostItem(NewsPostItem::TYPE_VIDEO, null,
-                null, null, null, $videoId));
         }
     }
 
@@ -289,42 +259,20 @@ class RusskiyVestnikParser implements ParserInterface
             return;
         }
 
-        //Get extremly bad formatted headers
-        if ($node->tagName === 'b' && empty($post->items) === true) {
-            $mainHeaderText = '';
-            foreach ($node->childNodes as $childNode) {
-                if ($childNode->tagName === 'font') {
-                    $textContent = self::cleanText($childNode->textContent);
-                    if (empty(trim($textContent)) === true) {
-                        return;
-                    }
-                    $post->addItem(new NewsPostItem(NewsPostItem::TYPE_HEADER, $textContent,
-                        null, null, 2));
-                } else {
-                    if (self::hasText($childNode)) {
-                        $textContent = self::cleanText($childNode->textContent);
-                        if (empty(trim($textContent)) === true) {
-                            return;
-                        }
-                        $mainHeaderText .= $textContent;
-                    }
-                }
-            }
-
-            $post->addItem(new NewsPostItem(NewsPostItem::TYPE_HEADER, $mainHeaderText,
-                null, null, 1));
-
-            return;
-        }
-
         //Get direct text nodes
         if (self::isText($node)) {
             if (self::hasText($node)) {
-                $textContent = self::cleanText($node->textContent);
-                if (empty(trim($textContent)) === true) {
-                    return;
+                $textContent = $node->textContent;
+                $textContent = self::cleanText($textContent);
+                if (strlen($post->description) >= strlen($textContent)) {
+                    if (preg_match('/' . preg_quote($textContent, '/') . '/', $post->description)) {
+                        return;
+                    }
                 }
-                $post->addItem(new NewsPostItem(NewsPostItem::TYPE_TEXT, $textContent));
+
+                if (self::hasActualText($textContent) === true) {
+                    $post->addItem(new NewsPostItem(NewsPostItem::TYPE_TEXT, $textContent));
+                }
             }
             return;
         }
@@ -340,11 +288,17 @@ class RusskiyVestnikParser implements ParserInterface
 
         //Get entire node text if we not need to parse any special entities, go recursive otherwise
         if ($needRecursive === false) {
-            $textContent = self::cleanText($node->textContent);
-            if (empty(trim($textContent)) === true) {
-                return;
+            $textContent = $node->textContent;
+            $textContent = self::cleanText($textContent);
+            if (strlen($post->description) >= strlen($textContent)) {
+                if (preg_match('/' . preg_quote($textContent, '/') . '/', $post->description)) {
+                    return;
+                }
             }
-            $post->addItem(new NewsPostItem(NewsPostItem::TYPE_TEXT, $textContent));
+
+            if (self::hasActualText($textContent) === true) {
+                $post->addItem(new NewsPostItem(NewsPostItem::TYPE_TEXT, $textContent));
+            }
         } else {
             foreach($node->childNodes as $child) {
                 self::parseNode($post, $child);
@@ -394,6 +348,18 @@ class RusskiyVestnikParser implements ParserInterface
                 $domNode->parentNode->removeChild($domNode);
             }
         });
+    }
+
+    /**
+     * Function check if string has actual text
+     * 
+     * @param string|null $text
+     * 
+     * @return bool
+     */
+    protected static function hasActualText(?string $text): bool
+    {
+        return trim($text, "⠀ \t\n\r\0\x0B\xC2\xA0") !== '';
     }
 
     /**
