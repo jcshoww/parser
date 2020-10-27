3fa3b3381e5bab82763591d5c0192737d0d579b3
jcshoww <corveynreigns@gmai.com>
2020-10-27 18:04:21 +0300

    SECREATEIO-11: Сделал парсер https://oane.ws/

---
+++ components/parser/news/OaNewsParser.php
@@ -0,0 +1,169 @@
+<?php
+
+namespace app\components\parser\news;
+
+use app\components\Helper;
+use app\components\parser\NewsPost;
+use app\components\parser\ParserInterface;
+use DateTime;
+use DateTimeZone;
+use DOMElement;
+use Exception;
+use Symfony\Component\DomCrawler\Crawler;
+use Symfony\Component\DomCrawler\UriResolver;
+
+/**
+ * News parser from site https://oane.ws/
+ * @author jcshow
+ */
+class OaNewsParser implements ParserInterface
+{
+    public const USER_ID = 2;
+
+    public const FEED_ID = 2;
+
+    public const SITE_URL = 'https://oane.ws';
+
+    /** @var array */
+    protected static $parsedEntities = ['a', 'img', 'blockquote', 'iframe', 'video'];
+
+    /**
+     * @inheritDoc
+     */
+    public static function run(): array
+    {
+        return self::getNewsData();
+    }
+
+    /**
+     * Function get fixed news count data
+     * 
+     * @param int $limit
+     * 
+     * @return array
+     * @throws Exception
+     */
+    public static function getNewsData(): array
+    {
+        /** Вырубаем нотисы */
+        error_reporting(E_ALL & ~E_NOTICE);
+
+        /** Get RSS news list */
+        $curl = Helper::getCurl();
+        $newsList = $curl->get(static::SITE_URL . "/rss.xml");
+        //Source has wrong XML :D
+        $newsList = preg_replace('/(version\=\"2\.0)(.*?)(xmlns.*\")(\")(\>)/', '$1"$2$3$5', $newsList);
+        if (! $newsList) {
+            throw new Exception('Can not get news data');
+        }
+
+        /** Parse news from RSS */
+        $posts = [];
+        $newsListCrawler = new Crawler($newsList);
+        $news = $newsListCrawler->filterXPath('//item');
+        foreach ($news as $item) {
+            $post = self::getPostDetail($item);
+
+            $posts[] = $post;
+        }
+
+        return $posts;
+    }
+
+    /**
+     * Function get post detail data
+     * 
+     * @param DOMElement $item
+     * 
+     * @return NewPost
+     */
+    public static function getPostDetail(DOMElement $item): NewsPost
+    {
+        $itemCrawler = new Crawler($item);
+
+        /** Get item detail link */
+        $link = self::cleanUrl($itemCrawler->filterXPath('//link')->text());
+
+        /** Get title */
+        $title = self::cleanText($itemCrawler->filterXPath('//title')->text());
+
+        /** Get item datetime */
+        $createdAt = new DateTime($itemCrawler->filterXPath('//pubDate')->text());
+        $createdAt->setTimezone(new DateTimeZone('UTC'));
+        $createdAt = $createdAt->format('c');
+
+        //Get image if exists
+        $picture = null;
+        $imageBlock = $itemCrawler->filterXPath('//enclosure')->getNode(0);
+        if (! empty($imageBlock) === true) {
+            $picture = self::cleanUrl($imageBlock->getAttribute('url'));
+            if (! preg_match('/http[s]?/', $picture)) {
+                $picture = UriResolver::resolve($picture, static::SITE_URL);
+            }
+        }
+
+        /** Get description */
+        $descriptionBlock = $itemCrawler->filterXPath('//yandex:full-text')->getNode(0);
+        $description = self::cleanText($descriptionBlock->textContent);
+
+        /** @var NewsPost */
+        $post = new NewsPost(static::class, $title, $description, $createdAt, $link, $picture);
+
+        return $post;
+    }
+
+    /**
+     * Function cleans text from bad symbols
+     * 
+     * @param string $text
+     * 
+     * @return string|null
+     */
+    protected static function cleanText(string $text): ?string
+    {
+        $transformedText = preg_replace('/(^[\r\n]*|[\r\n]+)[\s\t]*[\r\n]+/m', '', $text);
+        $transformedText = preg_replace('/\<script.*\<\/script>/m', '', $transformedText);
+        $transformedText = mb_convert_encoding($transformedText, 'UTF-8', mb_detect_encoding($transformedText));
+        $transformedText = html_entity_decode($transformedText);
+        $transformedText = preg_replace('/^\p{Z}+|\p{Z}+$/u', '', htmlspecialchars_decode($transformedText));
+        $transformedText = preg_replace('/[\x00-\x09\x0B\x0C\x0E-\x1F\x7F]/m', '', $transformedText);
+        $transformedText = preg_replace('/\xe2\xa0\x80/m', '', $transformedText);
+        return $transformedText;
+    }
+
+    /**
+     * Function clean dangerous urls
+     * 
+     * @param string $url
+     * 
+     * @return string
+     */
+    protected static function cleanUrl(string $url): string
+    {
+        return preg_replace_callback('/[^\x21-\x7f]/', function ($match) {
+            return rawurlencode($match[0]);
+        }, $url);
+    }
+
+    /**
+     * Function remove useless specified nodes
+     * 
+     * @param Crawler $crawler
+     * @param string $xpath
+     * @param int|null $count
+     * 
+     * @return void
+     */
+    protected static function removeNodes(Crawler $crawler, string $xpath, ?int $count = null): void
+    {
+        $crawler->filterXPath($xpath)->each(function (Crawler $crawler, int $key) use ($count) {
+            if ($count !== null && $key === $count) {
+                return;
+            }
+            $domNode = $crawler->getNode(0);
+            if ($domNode) {
+                $domNode->parentNode->removeChild($domNode);
+            }
+        });
+    }
+} 
