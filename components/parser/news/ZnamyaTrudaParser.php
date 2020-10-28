ed54a564ba5b793804853a25e50df9261eec2b83
jcshoww <corveynreigns@gmai.com>
2020-10-28 08:51:53 +0300

    SECREATEIO-11: Переделал работу парсера

--- components/parser/news/ZnamyaTrudaParser.php
+++ components/parser/news/ZnamyaTrudaParser.php
@@ -8,9 +8,10 @@ use app\components\parser\NewsPostItem;
 use app\components\parser\ParserInterface;
 use DateTime;
 use DateTimeZone;
+use DOMNode;
 use Exception;
-use lanfix\parser\Parser;
-use lanfix\parser\src\Element;
+use Symfony\Component\DomCrawler\Crawler;
+use Symfony\Component\DomCrawler\UriResolver;
 
 /**
  * News parser from site https://gazeta-trud.ru/
@@ -24,8 +25,8 @@ class ZnamyaTrudaParser implements ParserInterface
 
     public const SITE_URL = 'https://gazeta-trud.ru';
 
-    /** @var bool */
-    public static $needNextContainer = false;
+    /** @var array */
+    protected static $parsedEntities = ['a', 'img', 'blockquote', 'iframe', 'video'];
 
     /**
      * @inheritDoc
@@ -64,7 +65,11 @@ class ZnamyaTrudaParser implements ParserInterface
         $result = [];
         $news = $news['results']['objects'];
         foreach ($news as $item) {
-            $result[] = self::getPostDetail($item);
+            try {
+                $result[] = self::getPostDetail($item);
+            } catch (Exception $e) {
+                continue;
+            }
         }
 
         return $result;
@@ -93,94 +98,323 @@ class ZnamyaTrudaParser implements ParserInterface
         $createdAt->setTimezone(new DateTimeZone('UTC'));
         $createdAt = $createdAt->format('c');
 
-        /** Get item preview picture */
-        $mediaParser = new Parser($item['media'], true);
-        $photoUrl = '';
-        if ($photoHtmlNode = $mediaParser->document->getBody()->findOne('img')) {
-            $photoUrl = static::SITE_URL . trim($photoHtmlNode->getAttribute('src') ?: '');
-        }
-
-        /** @var NewsPost */
-        $post = new NewsPost(static::class, $title, $description, $createdAt, $link, $photoUrl);
-
         /**
          * Detail page parser creation
          */
         $curl = Helper::getCurl();
         $curlResult = $curl->get($link);
-        $detailPageParser = new Parser($curlResult, true);
-        $detailPageBody = $detailPageParser->document->getBody();
+        $crawler = new Crawler($curlResult);
 
-        /** Skip if no content */
-        if ($content = $detailPageBody->findOne('.content-body .content')) {
-            self::appendPostAdditionalData($post, $content);
+        self::removeNodes($crawler, '//comment() | //br | //hr | //script | //style | //form 
+        | //header | //head | //noscript');
+
+        /** Get item preview picture */
+        $imageBlock = $crawler->filterXpath('//div[contains(@class,"media-block")]//img')->getNode(0);
+        $picture = '';
+        if (! empty($imageBlock) === true) {
+            $picture = self::cleanUrl($imageBlock->getAttribute('src'));
+            if (! preg_match('/http[s]?/', $picture)) {
+                $picture = UriResolver::resolve($picture, static::SITE_URL);
+            }
+        }
+
+        /** @var NewsPost */
+        $post = new NewsPost(static::class, $title, $description, $createdAt, $link, $picture);
+
+        $detailPage = $crawler->filterXPath('
+            //div[contains(@class,"content-body")]
+            //div[contains(@class,"content")]
+            //div[contains(@class,"theme-default")]
+        ')->getNode(0);
+
+        // parse detail page for texts
+        foreach ($detailPage->childNodes as $node) {
+            self::parseNode($post, $node);
         }
 
         return $post;
     }
 
     /**
-     * Function appends NewsPostItem objects to NewsPost with additional post data
+     * Function parse single children of full text block and appends NewsPostItems founded
      * 
      * @param NewsPost $post
-     * @param Element $content
+     * @param DOMNode $node
+     * @param bool $skipText
      * 
      * @return void
      */
-    public static function appendPostAdditionalData(NewsPost $post, Element $content): void
+    public static function parseNode(NewsPost $post, DOMNode $node, bool $skipText = false): void
     {
-        /** Parse item content */
-        $contentBlocks = $content->getChildren();
+        //Get non-empty quotes from nodes
+        if (self::isQuoteType($node) && self::hasText($node)) {
+            $post->addItem(new NewsPostItem(NewsPostItem::TYPE_QUOTE, $node->textContent));
+            return;
+        }
 
-        /** Next container appends, if owl-gallery used */
-        $bodyKey = 2;
-        do {
-            self::$needNextContainer = false;
-            if (isset($contentBlocks[$bodyKey])) {
-                /** Item body container with other data */
-                $itemBody = $contentBlocks[$bodyKey]->findOne('.theme-default');
-    
-                self::appendPostBody($post, $itemBody);
+        //Get non-empty images from nodes
+        if (self::isImageType($node)) {
+            $imageLink = self::cleanUrl($node->getAttribute('src'));
+
+            if ($imageLink === '') {
+                return;
             }
-            $bodyKey++;
-        } while (self::$needNextContainer === true);
+
+            if (! preg_match('/http[s]?/', $imageLink)) {
+                $imageLink = UriResolver::resolve($imageLink, static::SITE_URL);
+            }
+
+            $post->addItem(new NewsPostItem(NewsPostItem::TYPE_IMAGE, $node->getAttribute('alt'), $imageLink));
+            return;
+        }
+
+        //Get videos from text
+        if (self::isVideoType($node)) {
+            $link = self::cleanUrl($node->getAttribute('src'));
+            if ($link && $link !== '') {
+                if ($ytVideoId = self::getYoutubeVideoId($link)) {
+                    $post->addItem(new NewsPostItem(NewsPostItem::TYPE_VIDEO, null, null, null, null, $ytVideoId));
+                    return;
+                }
+                if (! preg_match('/http[s]?/', $link)) {
+                    $link = UriResolver::resolve($link, static::SITE_URL);
+                }
+                if (preg_match('/vk\.com/', $link)) {
+                    $link = preg_replace('/^(\/\/)(.*)/', 'https://$2', html_entity_decode($link));
+                }
+                if (filter_var($link, FILTER_VALIDATE_URL)) {
+                    $post->addItem(new NewsPostItem(NewsPostItem::TYPE_LINK, null, null, $link));
+                }
+            }
+            return;
+        }
+
+        //Get non-empty links from nodes
+        if (self::isLinkType($node) && self::hasText($node)) {
+            $link = self::cleanUrl($node->getAttribute('href'));
+            if ($link && $link !== '') {
+                if ($ytVideoId = self::getYoutubeVideoId($link)) {
+                    $post->addItem(new NewsPostItem(NewsPostItem::TYPE_VIDEO, null, null, null, null, $ytVideoId));
+                    return;
+                }
+                if (! preg_match('/http[s]?/', $link)) {
+                    $link = UriResolver::resolve($link, static::SITE_URL);
+                }
+                if (filter_var($link, FILTER_VALIDATE_URL)) {
+                    $linkText = self::hasText($node) ? $node->textContent : null;
+                    $linkText = self::cleanText($linkText);
+                    $post->addItem(new NewsPostItem(NewsPostItem::TYPE_LINK, $linkText, null, $link));
+                }
+            }
+            return;
+        }
+
+        //Get direct text nodes
+        if (self::isText($node)) {
+            if ($skipText === false && self::hasText($node)) {
+                $textContent = self::cleanText($node->textContent);
+
+                if (self::hasActualText($textContent) === true) {
+                    $post->addItem(new NewsPostItem(NewsPostItem::TYPE_TEXT, $textContent));
+                }
+            }
+            return;
+        }
+
+        //Check if some required to parse entities exists inside node
+        $needRecursive = false;
+        foreach (self::$parsedEntities as $entity) {
+            if ($node->getElementsByTagName("$entity")->length > 0) {
+                $needRecursive = true;
+                break;
+            }
+        }
+
+        //Get entire node text if we not need to parse any special entities, go recursive otherwise
+        if ($skipText === false && $needRecursive === false) {
+            $textContent = self::cleanText($node->textContent);
+
+            if (self::hasActualText($textContent) === true) {
+                $post->addItem(new NewsPostItem(NewsPostItem::TYPE_TEXT, $textContent));
+            }
+        } else {
+            foreach($node->childNodes as $child) {
+                self::parseNode($post, $child, $skipText);
+            }
+        }
     }
 
     /**
-     * Function appends post body to post
+     * Function cleans text from bad symbols
      * 
-     * @param NewsPost $post
-     * @param Element $body
+     * @param string $text
+     * 
+     * @return string|null
+     */
+    protected static function cleanText(string $text): ?string
+    {
+        $transformedText = preg_replace('/(^[\r\n]*|[\r\n]+)[\s\t]*[\r\n]+/m', '', $text);
+        $transformedText = preg_replace('/\<script.*\<\/script>/m', '', $transformedText);
+        $transformedText = mb_convert_encoding($transformedText, 'UTF-8', mb_detect_encoding($transformedText));
+        $transformedText = html_entity_decode($transformedText);
+        $transformedText = preg_replace('/^\p{Z}+|\p{Z}+$/u', '', htmlspecialchars_decode($transformedText));
+        $transformedText = preg_replace('/[\x00-\x09\x0B\x0C\x0E-\x1F\x7F]/m', '', $transformedText);
+        $transformedText = preg_replace('/\xe2\xa0\x80/m', '', $transformedText);
+        return $transformedText;
+    }
+
+    /**
+     * Function clean dangerous urls
+     * 
+     * @param string $url
+     * 
+     * @return string
+     */
+    protected static function cleanUrl(string $url): string
+    {
+        return preg_replace_callback('/[^\x21-\x7f]/', function ($match) {
+            return rawurlencode($match[0]);
+        }, $url);
+    }
+
+    /**
+     * Function check if string has actual text
+     * 
+     * @param string|null $text
+     * 
+     * @return bool
+     */
+    protected static function hasActualText(?string $text): bool
+    {
+        return trim($text, "⠀ \t\n\r\0\x0B\xC2\xA0") !== '';
+    }
+
+    /**
+     * Function check if node text content not empty
+     * 
+     * @param DOMNode $node
+     * 
+     * @return bool
+     */
+    protected static function hasText(DOMNode $node): bool
+    {
+        return trim($node->textContent) !== '';
+    }
+
+    /**
+     * Function check if node is <p></p>
+     * 
+     * @param DOMNode
+     * 
+     * @return bool
+     */
+    protected static function isParagraphType(DOMNode $node): bool
+    {
+        return isset($node->tagName) === true && $node->tagName === 'p';
+    }
+
+    /**
+     * Function check if node is quote
+     * 
+     * @param DOMNode
+     * 
+     * @return bool
+     */
+    protected static function isQuoteType(DOMNode $node): bool
+    {
+        return isset($node->tagName) === true && in_array($node->tagName, ['blockquote']);
+    }
+
+    /**
+     * Function check if node is <a></a>
+     * 
+     * @param DOMNode
+     * 
+     * @return bool
+     */
+    protected static function isLinkType(DOMNode $node): bool
+    {
+        return isset($node->tagName) === true && $node->tagName === 'a';
+    }
+
+    /**
+     * Function check if node is image
+     * 
+     * @param DOMNode
+     * 
+     * @return bool
+     */
+    protected static function isImageType(DOMNode $node): bool
+    {
+        return isset($node->tagName) === true && $node->tagName === 'img';
+    }
+
+    /**
+     * Function check if node is video
+     * 
+     * @param DOMNode
+     * 
+     * @return bool
+     */
+    protected static function isVideoType(DOMNode $node): bool
+    {
+        return 
+            isset($node->tagName) === true && 
+            in_array(
+                $node->tagName,
+                ['iframe', 'video']
+            );
+    }
+
+    /**
+     * Function check if node is #text
+     * 
+     * @param DOMNode
+     * 
+     * @return bool
+     */
+    protected static function isText(DOMNode $node): bool
+    {
+        return $node->nodeName === '#text';
+    }
+
+    /**
+     * Function parse's out youtube video id from link
+     * 
+     * @param string $link
+     * 
+     * @return string|null
+     */
+    protected static function getYoutubeVideoId(string $link): ?string
+    {
+        preg_match(
+            '/(youtu\.be\/|youtube\.com\/(watch\?(.*&)?v=|(embed|v)\/))([\w-]{11})/iu',
+            $link,
+            $matches
+        );
+
+        return $matches[5] ?? null;
+    }
+
+    /**
+     * Function remove useless specified nodes
+     * 
+     * @param Crawler $crawler
+     * @param string $xpath
+     * @param int|null $count
      * 
      * @return void
      */
-    public static function appendPostBody(NewsPost $post, Element $itemBody): void
+    protected static function removeNodes(Crawler $crawler, string $xpath, ?int $count = null): void
     {
-        foreach ($itemBody->getChildren() ?: [] as $bodyBlock) {
-            if ($bodyBlock->tag === 'p') {
-                if ($video = $bodyBlock->findOne('iframe')) {
-                    $video = trim($video->getAttribute('src') ?: '');
-                    $videoId = preg_replace("/(.+)(\/)([^\/]+)$/", '$3', $video);
-                    $post->addItem(new NewsPostItem(NewsPostItem::TYPE_VIDEO, null,
-                        null, null, null, $videoId));
-                } else {
-                    if (! empty(trim($bodyBlock->asText())) === true) {
-                        $post->addItem(new NewsPostItem(NewsPostItem::TYPE_TEXT, $bodyBlock->asText(),
-                            null, null, null, null));
-                    }
-                }
-            } elseif ($bodyBlock->tag === 'blockquote') {
-                $post->addItem(new NewsPostItem(NewsPostItem::TYPE_QUOTE, $bodyBlock->asText(),
-                    null, null, null, null));
-            } elseif ($bodyBlock->tag === 'div' && in_array('publication-carousel', $bodyBlock->classes)) {
-                self::$needNextContainer = true;
-                $carousel = $bodyBlock->findOne('.owl-carousel');
-                foreach ($carousel->getChildren() ?: [] as $mediaItem) {
-                    $image = static::SITE_URL . trim($mediaItem->findOne('img')->getAttribute('src') ?: '');
-                    $post->addItem(new NewsPostItem(NewsPostItem::TYPE_IMAGE, null,
-                        $image, null, null, null));
-                }
+        $crawler->filterXPath($xpath)->each(function (Crawler $crawler, int $key) use ($count) {
+            if ($count !== null && $key === $count) {
+                return;
             }
-        }
+            $domNode = $crawler->getNode(0);
+            if ($domNode) {
+                $domNode->parentNode->removeChild($domNode);
+            }
+        });
     }
 }
