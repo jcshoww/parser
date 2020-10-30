b942781cde95a68047444d44bcabd2775b833514
jcshoww <corveynreigns@gmai.com>
2020-10-30 18:12:49 +0300

    SECREATEIO-11: Поправил описание/тело новости

--- components/parser/news/ArhInfoParser.php
+++ components/parser/news/ArhInfoParser.php
@@ -3,11 +3,14 @@
 namespace app\components\parser\news;
 
 use app\components\Helper;
+use app\components\helper\aayaami\DOMNodeRecursiveIterator;
 use app\components\parser\NewsPost;
+use app\components\parser\NewsPostItem;
 use app\components\parser\ParserInterface;
 use DateTime;
 use DateTimeZone;
 use DOMElement;
+use DOMNode;
 use Exception;
 use Symfony\Component\DomCrawler\Crawler;
 
@@ -94,7 +97,21 @@ class ArhInfoParser implements ParserInterface
         $createdAt = $createdAt->format('c');
 
         /** Get description */
-        $description = self::cleanText($itemCrawler->filterXPath('//yandex:full-text')->text());
+        $descriptionHtml = $itemCrawler->filterXPath('//turbo:content')->text();
+        $crawler = new Crawler($descriptionHtml);
+        $description = [];
+        $nodeIterator = new DOMNodeRecursiveIterator($crawler->getNode(0)->childNodes);
+        foreach ($nodeIterator->getRecursiveIterator() as $node) {
+            $text = self::cleanText($node->textContent);
+            $description[] = $text;
+            if (! empty($node->textContent)) {
+                $node->parentNode->removeChild($node);
+            }
+            if (substr($text, -1) === '.') {
+                break;
+            }
+        }
+        $description = implode(' ', $description);
 
         //Get image if exists
         $picture = null;
@@ -106,7 +123,104 @@ class ArhInfoParser implements ParserInterface
         /** @var NewsPost */
         $post = new NewsPost(static::class, $title, $description, $createdAt, $link, $picture);
 
+        // parse detail page for texts
+        foreach ($crawler->getNode(0)->childNodes as $node) {
+            self::parseNode($post, $node);
+        }
+
         return $post;
+    }
+
+        /**
+     * Function parse single children of full text block and appends NewsPostItems founded
+     * 
+     * @param NewsPost $post
+     * @param DOMNode $node
+     * @param bool $skipText
+     * 
+     * @return void
+     */
+    public static function parseNode(NewsPost $post, DOMNode $node, bool $skipText = false): void
+    {
+        //Get non-empty quotes from nodes
+        if (self::isQuoteType($node) && self::hasText($node)) {
+            $post->addItem(new NewsPostItem(NewsPostItem::TYPE_QUOTE, $node->textContent));
+            return;
+        }
+
+        //Get non-empty images from nodes
+        if (self::isImageType($node)) {
+            $imageLink = self::cleanUrl($node->getAttribute('src'));
+
+            if ($imageLink === '') {
+                return;
+            }
+
+            $imageLink = UriResolver::resolve($imageLink, static::SITE_URL);
+
+            $post->addItem(new NewsPostItem(NewsPostItem::TYPE_IMAGE, $node->getAttribute('alt'), $imageLink));
+            return;
+        }
+
+        //Get non-empty links from nodes
+        if (self::isLinkType($node) && self::hasText($node)) {
+            $link = self::cleanUrl($node->getAttribute('href'));
+            if ($link && $link !== '') {
+                if (! preg_match('/http[s]?/', $link)) {
+                    $link = UriResolver::resolve($link, static::SITE_URL);
+                }
+                if (filter_var($link, FILTER_VALIDATE_URL)) {
+                    $linkText = self::hasText($node) ? $node->textContent : null;
+                    $linkText = self::cleanText($linkText);
+                    $post->addItem(new NewsPostItem(NewsPostItem::TYPE_LINK, $linkText, null, $link));
+                }
+            }
+            return;
+        }
+
+        //Get direct text nodes
+        if (self::isText($node)) {
+            if ($skipText === false && self::hasText($node)) {
+                $textContent = self::cleanText($node->textContent);
+                if (strlen($post->description) >= strlen($textContent)) {
+                    if (preg_match('/' . preg_quote($textContent, '/') . '/', $post->description)) {
+                        return;
+                    }
+                }
+
+                if (self::hasActualText($textContent) === true) {
+                    $post->addItem(new NewsPostItem(NewsPostItem::TYPE_TEXT, $textContent));
+                }
+            }
+            return;
+        }
+
+        //Check if some required to parse entities exists inside node
+        $needRecursive = false;
+        foreach (self::$parsedEntities as $entity) {
+            if ($node->getElementsByTagName("$entity")->length > 0) {
+                $needRecursive = true;
+                break;
+            }
+        }
+
+        //Get entire node text if we not need to parse any special entities, go recursive otherwise
+        if ($skipText === false && $needRecursive === false) {
+            $textContent = self::cleanText($node->textContent);
+            if (strlen($post->description) >= strlen($textContent)) {
+                if (preg_match('/' . preg_quote($textContent, '/') . '/', $post->description)) {
+                    return;
+                }
+            }
+
+            if (self::hasActualText($textContent) === true) {
+                $post->addItem(new NewsPostItem(NewsPostItem::TYPE_TEXT, $textContent));
+            }
+        } else {
+            foreach($node->childNodes as $child) {
+                self::parseNode($post, $child, $skipText);
+            }
+        }
     }
 
     /**
@@ -135,5 +249,111 @@ class ArhInfoParser implements ParserInterface
     {
         $url = urlencode($url);
         return str_replace(array('%3A', '%2F'), array(':', '/'), $url);
+    }
+
+    /**
+     * Function check if string has actual text
+     * 
+     * @param string|null $text
+     * 
+     * @return bool
+     */
+    protected static function hasActualText(?string $text): bool
+    {
+        return trim($text, "⠀ \t\n\r\0\x0B\xC2\xA0") !== '';
+    }
+
+    /**
+     * Function check if node text content not empty
+     * 
+     * @param DOMNode $node
+     * 
+     * @return bool
+     */
+    protected static function hasText(DOMNode $node): bool
+    {
+        return trim($node->textContent) !== '';
+    }
+
+    /**
+     * Function check if node is <p></p>
+     * 
+     * @param DOMNode
+     * 
+     * @return bool
+     */
+    protected static function isParagraphType(DOMNode $node): bool
+    {
+        return isset($node->tagName) === true && $node->tagName === 'p';
+    }
+
+    /**
+     * Function check if node is quote
+     * 
+     * @param DOMNode
+     * 
+     * @return bool
+     */
+    protected static function isQuoteType(DOMNode $node): bool
+    {
+        return isset($node->tagName) === true && in_array($node->tagName, ['blockquote']);
+    }
+
+    /**
+     * Function check if node is <a></a>
+     * 
+     * @param DOMNode
+     * 
+     * @return bool
+     */
+    protected static function isLinkType(DOMNode $node): bool
+    {
+        return isset($node->tagName) === true && $node->tagName === 'a';
+    }
+
+    /**
+     * Function check if node is image
+     * 
+     * @param DOMNode
+     * 
+     * @return bool
+     */
+    protected static function isImageType(DOMNode $node): bool
+    {
+        return isset($node->tagName) === true && $node->tagName === 'img';
+    }
+
+    /**
+     * Function check if node is #text
+     * 
+     * @param DOMNode
+     * 
+     * @return bool
+     */
+    protected static function isText(DOMNode $node): bool
+    {
+        return $node->nodeName === '#text';
+    }
+
+    /**
+     * Function remove useless specified nodes
+     * 
+     * @param Crawler $crawler
+     * @param string $xpath
+     * @param int|null $count
+     * 
+     * @return void
+     */
+    protected static function removeNodes(Crawler $crawler, string $xpath, ?int $count = null): void
+    {
+        $crawler->filterXPath($xpath)->each(function (Crawler $crawler, int $key) use ($count) {
+            if ($count !== null && $key === $count) {
+                return;
+            }
+            $domNode = $crawler->getNode(0);
+            if ($domNode) {
+                $domNode->parentNode->removeChild($domNode);
+            }
+        });
     }
 } 
